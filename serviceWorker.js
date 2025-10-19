1debfe56625386e9a5cc3ed5ab7928f4299ecdba
pd2 <its.pdp@gmail.com>
2025-10-19 15:57:14 +0100

    Initial commit

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticDev = "panchang_puzzle"
+const service_worker_version = "v1.0103"
+const assets = [
+  "/",
+  "index.html",
+  "style.css",
+  "script.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
